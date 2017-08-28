bfa35b275510c008e9e78600fcac5d63b3f2fbac
Flatiron School <flatironschool@Flatirons-MacBook-Pro.local>
2017-08-28 11:34:22 -0400

    updating tests and solution

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ class Driver {
     this.id = ++driverId
     store.drivers.push(this)
   }
+  trips(){
+    return store.trips.filter((trip)=> {
+      return trip.driverId == this.id
+    })
+  }
+  passengers(){
+    return this.trips().map((trip)=> {
+      return trip.passenger()
+    })
+  }
 }
 
 let passengerId = 0
@@ -24,12 +34,31 @@ class Passenger {
     this.id = ++passengerId
     store.passengers.push(this)
   }
+
+  trips(){
+    return store.trips.filter((trip)=> {
+      return trip.passengerId == this.id
+    })
+  }
+  drivers(){
+    return this.trips().map((trip)=> {
+      return trip.driver()
+    })
+  }
 }
 
 let tripId = 0
 class Trip {
-  constructor(startingZip, endingZip){
+  constructor(driver, passenger, startingZip, endingZip){
+    this.driverId = driverId;
+    this.passengerId = passengerId;
     this.id = ++tripId
     store.trips.push(this)
+  }
+  driver(){
+    return store.drivers.find((driver) => { return driver.id === this.driverId })
+  }
+  passenger(){
+    return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
   }
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -77,10 +77,7 @@ describe('trips', function() {
       })
     })
 
-    it('can create a Trip with a name', function() {
-      // let trip = new Passenger("Alfie")
-      // expect(trip.name).to.equal("Alfie")
-    })
+
 
     it('adds the trip to the store', function() {
       store.trips = []
@@ -99,6 +96,68 @@ describe('trips', function() {
       let trip = new Trip()
       let otherTrip = new Trip()
       expect(trip.id).to.not.equal(otherTrip.id)
+    })
+  })
+})
+
+describe('relating a driver to a trip and a passenger', function() {
+  let driver;
+  let passenger;
+  let firstTrip;
+  let susan;
+  let secondTrip;
+
+  beforeEach(function() {
+    driver = new Driver("Alfie")
+    passenger = new Passenger("Bob")
+    firstTrip = new Trip(driver, passenger)
+    secondPassenger = new Passenger("Susan")
+    secondTrip = new Trip(driver, secondPassenger)
+  });
+
+  afterEach(function(){
+    store.drivers = []
+    store.passengers = []
+    store.trips = []
+  })
+
+  describe('trip', function(){
+    it('has a driverId', function(){
+      expect(firstTrip.driverId).to.equal(driver.id)
+    })
+
+    it('has a passengerId', function() {
+      expect(firstTrip.passengerId).to.equal(passenger.id)
+    })
+
+    it('has a passenger', function() {
+      expect(firstTrip.passenger()).to.equal(passenger)
+    })
+
+    it('has a driver', function() {
+      expect(firstTrip.driver()).to.equal(driver)
+    })
+  })
+
+  describe('driver', function(){
+    it('has a trips', function() {
+      expect(driver.trips()).to.include(firstTrip)
+      expect(driver.trips()).to.include(secondTrip)
+    })
+
+    it('has passengers', function() {
+      expect(driver.passengers()).to.include(passenger)
+      expect(driver.passengers()).to.include(secondPassenger)
+    })
+  })
+
+  describe('passengers', function(){
+    it('has a trips', function() {
+      expect(passenger.trips()).to.include(firstTrip)
+    })
+
+    it('has drivers', function() {
+      expect(passenger.drivers()).to.include(driver)
     })
   })
 })
