bb19e53ebedf8ee89165c590e9b187184c249f2a
Flatiron School <flatironschool@Flatirons-MacBook-Pro.local>
2017-08-28 16:18:35 -0400

    adding solution

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 ## Instructions
 
-In the following lab, we want to model our Scuber system in such a way that we can see all of the passengers related to a driver.  A driver becomes related to a passenger when he takes a single trip.  Only one passenger is associated with a trip -- the passenger who books, and pays for the trip on their phone.
+In the following lab, we will be modeling a the models for The Daily Deli.  The Daily Deli delivers daily lunches to different offices around US Cities.
 
-Now, for each class that you create, we will first need an incrementing id to keep track of each individual instance of the respective class.  Then we recommending building out the relationship methods.
+ You will be modeling the following:
+
+ An customer belongs to an employer.  An employer has many customers.  A delivery belongs to a meal and a customer on a day.  
--- index.js
+++ index.js
@@ -1,64 +1,108 @@
-// driver has many trips
-// passenger has many trips
-// trips
-  // passenger
-  // driver
-  // belongs to scooter
+let store = {deliveries: [], employers: [], customers: [], meals: []}
 
-let store = {drivers: [], passengers: [], trips: []}
+let mealId = 0
 
-let driverId = 0
-class Driver {
-  constructor(name, zipcode){
-    this.name = name
-    this.id = ++driverId
-    store.drivers.push(this)
+class Meal {
+  constructor(title, price){
+    this.title = title
+    this.price = price
+    this.id = ++mealId
+    store.meals.push(this)
   }
-  trips(){
-    return store.trips.filter((trip)=> {
-      return trip.driverId == this.id
+  deliveries(){
+    return store.deliveries.filter((delivery)=> {
+      return delivery.mealId == this.id
     })
   }
-  passengers(){
-    return this.trips().map((trip)=> {
-      return trip.passenger()
+  customers(){
+    return this.deliveries().map((delivery)=> {
+      return delivery.customer()
+    })
+  }
+  static byPrice(){
+    return store.meals.sort((meal) => { return meal.price });
+  }
+}
+
+let customerId = 0
+class Customer {
+  constructor(name, employer = {} ){
+    this.name = name;
+    this.employerId = employer.id;
+    this.id = ++customerId
+    store.customers.push(this)
+  }
+  totalSpent(){
+    return this.meals().reduce(function(sum, meal) { return sum + meal.price }, 0)
+  }
+  deliveries(){
+    return store.deliveries.filter((delivery)=> {
+      return delivery.customerId == this.id
+    })
+  }
+  meals(){
+    return this.deliveries().map((delivery)=> {
+      return delivery.meal()
     })
   }
 }
 
-let passengerId = 0
 
-class Passenger {
+let deliveryId = 0
+class Delivery {
+  constructor(meal = {}, customer = {}){
+    this.mealId = meal.id;
+    this.customerId = customer.id;
+    this.id = ++deliveryId
+    store.deliveries.push(this)
+  }
+  meal(){
+    return store.meals.find((meal) => { return meal.id === this.mealId })
+  }
+  customer(){
+    return store.customers.find((customer) => { return customer.id === this.customerId })
+  }
+}
+
+
+let employerId = 0
+
+class Employer {
   constructor(name){
     this.name = name
-    this.id = ++passengerId
-    store.passengers.push(this)
+    this.id = ++employerId
+    store.employers.push(this)
   }
-
-  trips(){
-    return store.trips.filter((trip)=> {
-      return trip.passengerId == this.id
+  mealTotals(){
+    let allMeals = this.deliveries().map((delivery) => {
+      return delivery.meal()
+    })
+    let summaryObject = {}
+    allMeals.forEach(function(meal){
+      summaryObject[meal.id] = 0
+    })
+    allMeals.forEach(function(meal){
+      summaryObject[meal.id] += 1
+    })
+    return summaryObject;
+  }
+  employees(){
+    return store.customers.filter((customer)=> {
+      return customer.employerId == this.id
     })
   }
-  drivers(){
-    return this.trips().map((trip)=> {
-      return trip.driver()
+  deliveries(){
+    let allDeliveries = this.employees().map((employee)=> {
+      return employee.deliveries()
     })
+    let merged = [].concat.apply([], allDeliveries);
+    return merged;
   }
-}
-
-let tripId = 0
-class Trip {
-  constructor(driver, passenger, startingZip, endingZip){
-    this.driverId = driverId;
-    this.passengerId = passengerId;
-    this.id = ++tripId
-    store.trips.push(this)
-  }
-  driver(){
-    return store.drivers.find((driver) => { return driver.id === this.driverId })
-  }
-  passenger(){
-    return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
+  meals(){
+    let allMeals = this.deliveries().map((delivery)=> {
+      return delivery.meal()
+    })
+    let uniqueMeals = [...new Set(allMeals)]
+    return uniqueMeals;
   }
 }
