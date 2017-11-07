82635815851eee43736ccc91e5fd81fc058590c2
github username <logemerson@gmail.com>
2017-11-06 20:31:01 -0500

    Done.

---
+++ test/seed.js
@@ -0,0 +1,9 @@
+const c1 = new Customer("Logan", "ASU");
+const c2 = new Customer("Lauren", "UChicago");
+const c3 = new Customer("Rachel", "UChicago");
+const c4 = new Customer("Lawrence", "ASU");
+
+const m1 = new Meal("burger", 8);
+const m2 = new Meal("salad", 7);
+const m3 = new Meal("soup", 4);
+const m4 = new Meal("sandwich", 5);
