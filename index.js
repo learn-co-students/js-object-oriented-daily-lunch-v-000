ba6ef4f5c499f5b9e6852adad4ec2add856d4ff1
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-14 20:10:02 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,132 @@
+Array.prototype.unique = function() {
+  return this.filter(function (value, index, self) {
+    return self.indexOf(value) === index;
+  });
+}
+
+let store = {deliveries: [], meals: [], employers: [], customers: []};
+
+let deliveryId = 0;
+let mealId = 0;
+let employerId = 0;
+let customerId = 0;
+
+class Delivery {
+  constructor(meal, customer) {
+    this.id = ++deliveryId;
+    if (meal && customer) {
+      this.mealId = meal.id;
+      this.customerId = customer.id;
+    }
+    store.deliveries.push(this);
+  }
+
+  customer() {
+    return store.customers.find(customer => {
+      return customer.id === this.customerId;
+    });
+  }
+
+  meal() {
+    return store.meals.find(meal => {
+      return meal.id === this.mealId;
+    });
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+    store.meals.push(this);
+  }
+
+  static byPrice() {
+    return store.meals.slice().sort((a, b) => {
+      return b.price - a.price;
+    });
+  }
+
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id;
+    });
+  }
+
+  customers() {
+    return store.customers.filter(customer => {
+      return customer.deliveries().filter(delivery => {
+        return delivery.mealId === this.id;
+      });
+    })
+  }
+
+}
+
+class Customer {
+  constructor(name, employer) {
+    this.id = ++customerId;
+    this.name = name;
+    if (employer) {
+      this.employerId = employer.id;
+    }
+    store.customers.push(this);
+  }
+
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id;
+    });
+  }
+
+  meals() {
+    return this.deliveries().map(delivery => {
+      return delivery.meal();
+    });
+  }
+
+  totalSpent() {
+    return this.meals().reduce((sum, meal) => {
+      return sum + meal.price;
+    }, 0);
+  }
+}
+
+class Employer {
+  constructor(name) {
+    this.id = ++employerId;
+    this.name = name;
+    store.employers.push(this);
+  }
+
+  employees() {
+    return store.customers.filter(customer => {
+      return customer.employerId === this.id;
+    });
+  }
+
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return this.employees().includes(delivery.customer());
+    });
+  }
+
+  meals() {
+    return this.deliveries().map(delivery => {
+      return delivery.meal();
+    }).unique();
+  }
+
+  mealTotals(){
+  return this.deliveries().reduce((allMeals, delivery) => {
+    if (delivery.mealId in allMeals) {
+      allMeals[delivery.mealId]++;
+    } else {
+      allMeals[delivery.mealId] = 1;
+    }
+    return allMeals;
+    }, {});
+  }
+
+}
