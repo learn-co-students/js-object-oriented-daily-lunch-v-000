f88d269a28664385d7b0395a7fff4c9a67f0a10e
Hannah Carnes <hecarnes@gmail.com>
2018-02-22 19:21:27 -0500

    done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let store = {deliveries: [], meals: [], employers: [], customers: []}
+let store = {
+  deliveries: [],
+  meals: [],
+  employers: [],
+  customers: []
+}
 
 let deliveryId = 0
 let mealId = 0
@@ -8,22 +13,20 @@ let customerId = 0
 class Delivery {
   constructor(meal, customer) {
     this.id = ++deliveryId
-    if(customer) {
+    if (customer) {
       this.customerId = customer.id
       this.mealId = meal.id
     }
 
-    store.deliveries.push(this) 
+    store.deliveries.push(this)
   }
 
-  // delivery belongs to customer
   customer() {
-    return store.customers.find( customer => customer.id == this.customerId )
+    return store.customers.find(customer => customer.id == this.customerId)
   }
 
-  // delivery belongs to meal
   meal() {
-    return store.meals.find( meal => meal.id == this.mealId )
+    return store.meals.find(meal => meal.id == this.mealId)
   }
 }
 
@@ -37,12 +40,12 @@ class Meal {
   }
 
   static byPrice() {
-    return store.meals.concat().sort( (a,b) => b.price - a.price)
+    return store.meals.concat().sort((a, b) => b.price - a.price)
   }
 
   // meal belongs to delivery
   deliveries() {
-    return store.deliveries.filter( delivery => delivery.mealId == this.id )
+    return store.deliveries.filter(delivery => delivery.mealId == this.id)
   }
 
   // meal has customers through deliveries
@@ -54,37 +57,44 @@ class Meal {
 }
 
 class Employer {
-  constructor(name){
+  constructor(name) {
     this.id = ++employerId
     this.name = name
 
     store.employers.push(this)
   }
 
-  //customer/employee belongs to employer
   employees() {
-    return store.customers.filter( customer => customer.employerId == this.id)
+    return store.customers.filter(customer => customer.employerId == this.id)
   }
 
-  //employer has deliveries through employees/customers
   deliveries() {
-    return this.employees().map(customer => {
-      return customer.deliveries();
-    })
+    let employeeIds = this.employees().map(employee => employee.id)
+    return store.deliveries.filter(delivery => employeeIds.includes(delivery.customerId))
   }
 
-  //employer has meals through deliveries
-  // does not return the same meal twice
   meals() {
-    return [...new Set(this.deliveries().map(customer => customer.meal()))]
-    }
+    return [...new Set(this.deliveries().map(delivery => delivery.meal()))]
   }
+
+  allMeals() {
+    return this.deliveries().map(delivery => delivery.meal())
+  }
+
+  mealTotals() {
+    return this.allMeals().reduce((accm, meal) => {
+      accm[meal.id] = (accm[meal.id] || 0) + 1;
+      return accm;
+    }, {})
+  }
+
+}
 
 class Customer {
-  constructor(name, employer){
+  constructor(name, employer) {
     this.id = ++customerId
     this.name = name
-    if(employer) {
+    if (employer) {
       this.employerId = employer.id
     }
 
@@ -96,11 +106,10 @@ class Customer {
   }
 
   deliveries() {
-    return store.deliveries.filter( delivery => delivery.customerId == this.id)
+    return store.deliveries.filter(delivery => delivery.customerId == this.id)
   }
 
   meals() {
-    return store.meals.filter( meal => meal.customerId == this.mealId)
+    return store.meals.filter(meal => meal.customerId == this.mealId)
   }
 }
-
