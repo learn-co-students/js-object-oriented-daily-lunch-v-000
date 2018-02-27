76dc0a1f257cdde453a86b336fdbcea80236829a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-27 17:46:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let store= {customers:[],meals:[],deliveries:[]}
+let store= {customers:[],meals:[],deliveries:[],employers:[]}
+let id1=0
 class Customer {
-  let id1= 0
   constructor(name,employer){
     this.name=name
     this.id=++id1
     this.employerId=employer.id
+    store.customers.push(this)
   }
   meals(){
     let newdeliveries=store.deliveries.filter(dilevery=>{return delivery.customerId===this.id})
@@ -15,7 +16,7 @@ class Customer {
 }
   deliveries(){return store.deliveries.filter(dilevery=>{return delivery.customerId===this.id})}
   totalSpent(){//take all the meals and add their prices.
-      let prices=meals().map(function(meal){return meal.totalSpent})
+      let prices=this.meals().map(function(meal){return meal.totalSpent})
       let total=0
       for (i=0;i<prices.length;i++){
         total += prices[i]
@@ -26,14 +27,15 @@ class Customer {
 //   return meals.reduce(function(agg,el,i,arr){return agg + el.price},0)
 // }
 }
+let id2=0
 class Meal {
-  let id2=0
-  constructor(title,price){
+    constructor(title,price){
     this.title=title
     this.price=price
     this.id=++id2
+    store.meals.push(this)
   }
-  deliveries(){return store.deliveries.filter(dilevery=>{return delivery.mealId===this.id})}
+  deliveries(){return store.deliveries.filter(function(delivery){return delivery.mealId===this.id})}
   customers(){
     let newdeliveries=store.deliveries.filter(delivery=>{return delivery.mealId===this.id})
     let customerids=newdeliveries.map(function(delivery){return delivery.customerId})
@@ -42,27 +44,37 @@ class Meal {
     })
   }
   static byPrice(){
-    const mealsCopy= [...meals]
-    mealsCopy.sort(meal1,meal2){
+    //meals=store.meals
+    //const mealsCopy= [...meals]
+    store.meals.sort(function(meal1,meal2){
       meal1.price-meal2.price
-    }
+    })
   }
 }
 let id3=0
 class Delivery {
   constructor(meal,customer){
     this.id=++id3
+    if (customer){this.customerId=customer.id}
+
+    if (meal){this.mealId=meal.id}
+    store.deliveries.push(this)
+  }
+  setCustomer(customer){
     this.customerId=customer.id
+  }
+  setMeal(meal){
     this.mealId=meal.id
   }
-  customer(){return store.customers.find(customer=>return customer.id===this.customerId)}
-  meal(){return store.meals.find(meal=>return meal.id===this.mealId)}
+  customer(){store.customers.find(customer=>customer.id===this.customerId)}
+  meal(){store.meals.find(meal=>meal.id===this.mealId)}
 }
 let id4=0
 class Employer {
   constructor(name){
     this.name=name
     this.id=++id4
+    store.employers.push(this)
   }
   employees(){return store.customers.filter(customer=>{return customer.employerId===this.id})}
   deliveries(){//linked through customers
@@ -70,7 +82,7 @@ class Employer {
     let customerids=newcustomers.map(function(customer){return customer.id}) // this step is different.
     return store.deliveries.filter(function(delivery){
       if (customerids.includes(delivery.customerId)){return delivery}
-    }
+    })
 }
   meals(){ //goes through customers then deliveries then meals
     let newcustomers=store.customers.filter(customer=>{return customer.employerId===this.id})
@@ -80,20 +92,20 @@ class Employer {
     return store.meals.filter(meal=>{
       if (newdeliveries.includes(meal.id)){return meal}
     })
-  }
+  })
 }
 
   mealTotals(){//WILL USING MEALS() and EMPLOYEES() RETURN VALUE WORK??
-    object={}
-    meals().forEach(function(meal){
+    let object={}
+    this.meals().forEach(function(meal){
       let customerids= employees().map(function(customer){return customer.id})
       let deliveriesForEmployersCustomers= store.deliveries.filter(delivery=>{
         if (customerids.includes(delivery.customerId)){return delivery}
       })
       let count=0
-      value= deliveriesForEmployersCustomers.forEach(function(delivery{
+      value= deliveriesForEmployersCustomers.forEach(function(delivery){
         if (delivery.mealId===meal.id){count+=1}
-      }))
+      })
       object[meal.id]= count
     })
   }
