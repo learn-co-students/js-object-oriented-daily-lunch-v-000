c968aebad771adeab9e24aba865d6b7b1be29c68
BenB <bblkmn5@gmail.com>
2018-03-15 11:50:47 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,133 @@
+store = {deliveries: [], meals: [], employers: [], customers: []};
+
+deliveryId = 0;
+mealId = 0;
+employerId = 0;
+customerId = 0;
+
+class Customer {
+    constructor(name, employer){
+        this.id = ++customerId;
+        this.name = name;
+        if(employer){
+        this.employerId = employer.id;
+        }
+        store.customers.push(this);
+    }
+
+    deliveries(){
+        return store.deliveries.filter(delivery => {
+            return delivery.customerId === this.id
+        });
+    }
+
+    meals(){
+        return this.deliveries().map(delivery => {
+            return delivery.meal()
+        });
+    }
+
+    totalSpent(){
+        return this.meals().reduce(function(total, meal){
+            return total += meal.price
+        }, 0);
+    }
+}
+
+class Meal {
+    constructor(title, price){
+        this.id = ++mealId;
+        this.title = title;
+        this.price = price;
+        store.meals.push(this);
+    }
+
+    deliveries(){
+        return store.deliveries.filter(delivery => {
+            return delivery.mealId === this.id;
+        });
+    }
+    
+    customers(){
+        return this.deliveries().map(delivery => {
+            return delivery.customer();
+        });
+    }
+
+    static byPrice(){
+        return store.meals.sort(function(meal1, meal2){
+            return meal2.price - meal1.price;
+        });
+    }
+}
+class Delivery {
+    constructor(meal, customer){
+        this.id = ++deliveryId;
+        if(customer){
+        this.customerId = customer.id;
+        }
+        if(meal){
+        this.mealId = meal.id;
+        }
+        store.deliveries.push(this);
+    }
+
+    meal(){
+        return store.meals.find(meal => {
+            return meal.id === this.mealId;
+        });
+    }
+
+    customer(){
+        return store.customers.find(customer => {
+            return customer.id === this.customerId
+        });
+    }
+}
+
+class Employer {
+    constructor(name){
+        this.id = ++employerId;
+        this.name = name;
+        store.employers.push(this);
+    }
+
+    employees(){
+        return store.customers.filter(customer => {
+            return customer.employerId === this.id;
+        });
+    }
+
+    deliveries(){
+        let array = [];
+        this.employees().forEach(employee => {array = [...employee.deliveries(), ... array]});
+
+        return array;
+    }
+
+    meals(){
+        let meals = [];
+        this.deliveries().forEach(function(delivery) {
+            if(!meals.includes(delivery.meal())) {
+                meals.push(delivery.meal());
+            }
+        });
+        return meals;
+    }
+
+    mealTotals(){
+        let total = {};
+        this.meals().forEach(function(meal) {
+            total[meal.id] = 0;
+
+        this.deliveries().forEach(function(delivery) {
+            if (delivery.meal() === meal) {
+                total[meal.id] += 1
+            }
+        })
+        }.bind(this));
+        return total;
+    }
+
+}
+
