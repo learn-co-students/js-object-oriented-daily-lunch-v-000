8592408b0c896e658588aee0e706983421ced5e2
github username <github email address>
2018-03-19 14:47:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let customerId = 0;
 let deliveryId = 0;
+let mealId = 0;
 
 let store = {customers: [], meals: [], employers: [], deliveries: []}
 
@@ -10,6 +11,16 @@ class Delivery {
     this.mealId = meal.id;
     this.customerId = customer.id;
   }
+  store.deliveries.push(this)
+}
+
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title
+    this.price = price
+  }
+  store.meals.push(this)
 }
 
 class Customer {
