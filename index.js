68bea57ddce8f90e562eba32d29f326bb00f9935
github username <github email address>
2018-03-19 14:17:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 
-let customerId = 0
+let customerId = 0;
+let deliveryId = 0;
 
 let store = {customers: [], meals: [], employers: [], deliveries: []}
+
+class Delivery {
+  constructor(meal, customer) {
+    this.id = ++deliveryId;
+    this.mealId = meal.id;
+    this.customerId = customer.id;
+  }
+}
 
 class Customer {
   constructor(name, employer) {
