4d878f2e7d0cee5a7f135abc88a9f6e343e4aabe
Dan Suarez <suarez.daniel101@gmail.com>
2018-04-18 05:22:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-let store = {meals: [], deliveries: [], customers: [], drivers: []};
+let store = {meals: [], deliveries: [], customers: [], drivers: [], employers: []};
 let mealId = 0;
 let deliveryId = 0;
 let customerId = 0;
 let driverId = 0;
+let employerId = 0;
 
 // Delivery` class:
 //   + `new Delivery()` — initialized with `meal` and `customer`; returns an object that has attributes of `mealId`, `customerId`, and `id`
@@ -79,7 +80,40 @@ class Meal{
       return delivery.mealId === this.id
     })
   }
-  static byPrice(){
-    return Meal.mealId.price;
+  static byPrice(price){
+    return Meal.price;
+  }
+}
+
+// `Employer` class:
+//   + `new Employer()` — initialized with `name`; returns an object that has attributes of `name` and `id`
+//   + `employees()` - returns a list of customers employed by the employer
+//   + `deliveries()` - returns a list of deliveries ordered by the employer's employees
+//   + `meals()` - returns a list of meals ordered by the employer's employees.  The method is to not return the same meal multiple times.
+//   + `mealTotals()` - returns a JavaScript object displaying each respective meal id ordered by the employer's employees.
+//   The keys of the JavaScript object are the meal ids and associated with each meal id is a value.
+//   For example, `employerOne.mealTotals()` returning an object of `{1: 4, 2: 3}` would mean that the meal with id of 1 was
+//   ordered by employerOne's employees four times, and the meal with id of 2 was ordered by employerOne's employees three times.
+
+class Employer{
+  constructor(name){
+    this.id = ++employerId;
+    this.name = name;
+    store.employers.push(this);
+  }
+  employees(){
+    return store.customers.filter(customer => {
+      return customer.employerId === this.id
+    })
+  }
+  deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.employerId === this.id
+    })
+  }
+  meals(){
+    return store.meals.filter(meal => {
+      return meal.customerId === this.id
+    })
   }
 }
