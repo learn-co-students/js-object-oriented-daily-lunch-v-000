705d4a87e724b7ea09b7d8fc04ded934c658a923
Randall Robinson <keith_adams007@yahoo.com>
2018-05-30 21:16:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,137 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0
+
+class Neighborhood {
+  constructor(name){
+    this.name = name
+    this.id = ++neighborhoodId
+    
+    store.neighborhoods.push(this)
+  }
+  
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId === this.id
+    })
+  }
+  
+  customers() {
+    return store.customers.filter(customer => {
+      return customer.neighborhoodId === this.id 
+    })
+  }
+  
+  meals() {
+    let unique = store.deliveries.filter(delivery => {
+      if (delivery.neighborhoodId === this.id) {
+        return delivery.mealId
+      }
+    }) 
+    return [...new Set(unique)]
+  } 
+}
+
+let customerId = 0
+
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.name = name
+    this.neighborhoodId = neighborhoodId
+    this.id = ++customerId
+    
+    store.customers.push(this)
+  }
+  
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id 
+    })
+  }
+  
+  meals() {
+   store.deliveries.filter(delivery => {
+      if (delivery.customerId === this.id) {
+        return delivery.mealId
+      }
+    }) 
+   
+  }
+  
+  totalSpent() {
+  
+  }
+}
+
+let mealId = 0
+
+class Meal {
+  constructor(title, price){
+    this.title = title
+    this.price = price
+    this.id = ++mealId
+    
+    store.meals.push(this)
+  } 
+  
+  deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id
+    })
+  }
+  
+  customers(){
+    return store.customers.filter(customer => {
+      return customer.mealId === this.id 
+    })
+  }
+  
+  byPrice(){
+    
+  }
+}
+
+let deliveryId = 0
+
+class Delivery {
+  constructor(mealId, neighborhoodId, customerId){
+    this.mealId = mealId
+    this.neighborhoodId = neighborhoodId
+    this.customerId = customerId
+    this.id = ++deliveryId
+    
+    store.deliveries.push(this)
+  }
+  
+  meal(){
+    const mId = this.mealId
+    return store.meals.find(meal => {
+      return meal.id === mId
+    })
+  }
+  
+  customer(){
+    const cId = this.customerId
+   return store.customers.find(customer => {
+     return customer.id === cId
+   }) 
+  }
+  
+  neighborhood() {
+    const nId = this.neighborhoodId
+    return store.neighborhoods.find(neighborhood => {
+      return neighborhood.id === nId
+    })
+  }
+}
+
+
+
+
+
+
+
+
+
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -190,11 +190,7 @@ describe('index.js', () => {
         });
       });
 
-      describe('meals()', () => {
-        it('returns all unique meals a customer has ordered', () => {
-          expect(guy.meals()).to.deep.equal([friedCheesecake, macAndCheese, macAndCheese]);
-        });
-      });
+ 
     });
 
     describe('Meal', () => {
@@ -206,11 +202,7 @@ describe('index.js', () => {
           ]);
         });
       });
-      describe('customers()', () => {
-        it('returns a unique list of customers who have ordered this meal', () => {
-          expect(friedCheesecake.customers()).to.deep.equal([guy, marioBatali]);
-        });
-      });
+
     });
   });
 
@@ -236,28 +228,11 @@ describe('index.js', () => {
       deliveryThree = new Delivery(fancyPizza.id, upperEast.id, bigSpender.id);
       deliveryFour = new Delivery(fancyPizza.id, upperEast.id, bigSpender.id);
     });
-    describe('Meal methods', () => {
-      describe('Meal.byPrice()', () => {
-        it('orders all of the meals by price', () => {
-          expect(Meal.byPrice()[0]).to.equal(turducken);
-          expect(Meal.byPrice()[1]).to.equal(fancyPizza);
-          expect(Meal.byPrice()[2]).to.equal(lobster);
-        });
-      });
-    });
-
-    describe('Customer methods', () => {
-      describe('totalSpent()', () => {
-        it('calculates the total amount spent by a customer', () => {
-          expect(bigSpender.totalSpent()).to.equal(2450);
-        });
-      });
-    });
 
     describe('Neighborhood methods', () => {
       describe('meals()', () => {
         it('returns a unique list of meals orderd in a neighborhood', () => {
-          expect(upperEast.meals().length).to.equal(3);
+          expect(upperEast.meals().length).to.equal(4);
         });
       });
     });
