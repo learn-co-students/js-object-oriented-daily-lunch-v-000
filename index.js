f39ecd1cc1523e0f2e07a6d1ed864fcec991b500
dwieb <dwayne.harmon@gmail.com>
2018-06-01 23:42:08 -0500

    Working progress

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhood_counter = 0
+
+class Neighborhood {
+  constructor(name) {
+  this.name = name
+  this.id = neighborhood_counter++
+  store["neighborhoods"].push(this)
+  }
+
+
+  meals() {
+    let meals = this.customers().map(customer => {
+      return customer.meals().filter(function(item, pos) {
+        return customer.meals().indexOf(item) == pos;
+      })
+    })
+    console.log(meals[0]);
+    return meals[0]
+  }
+
+}
