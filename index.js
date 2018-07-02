3c0ab4c44cd0ba2012f98c0a023dba4dd2a9623e
Lara Sanders <larasanders@gmail.com>
2018-07-02 17:38:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,103 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0
+let customerId = 0
+let mealId = 0
+let deliveryId = 0
+
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name
+    this.id = neighborhoodId++
+    store.neighborhoods.push(this)
+  }
+
+  customers() {
+      return store.customers.filter(customer => {
+        return customer.neighborhoodId === this.id
+      })
+    }
+
+    meals() {
+      const allMeals = this.deliveries().map(delivery => delivery.mealId)
+      // return allMeals.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+      const meals = Array.from(new Set(allMeals))
+      return meals
+    }
+
+      deliveries() {
+        return store.deliveries.filter(delivery => {
+          return delivery.neighborhoodId === this.id;
+        })
+      }
+    }
+
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.name = name
+    this.neighborhoodId = neighborhoodId
+    this.id = customerId++
+    store.customers.push(this)
+  }
+
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id
+
+    })
+  }
+  meals() {
+    return this.deliveries().map(delivery => delivery.meal())
+  }
+
+  totalSpent() {
+    return this.meals().reduce((total, meal) => (total += meal.price), 0)
+  }
+}
+
+class Meal {
+  constructor(title, price) {
+    this.title = title
+    this.price = price
+    this.id = mealId++
+    store.meals.push(this)
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id
+    })
+  }
+  customers() {
+      return store.customers.filter(customer => {
+        return customer.mealId === this.id.uniq
+      })
+    }
+  static byPrice() {
+    return store.meals.sort((a, b) => a.price < b.price)
+
+  }
+}
+class Delivery {
+  constructor(mealId, neighborhoodId, customerId) {
+    this.mealId = mealId
+    this.neighborhoodId = neighborhoodId
+    this.customerId = customerId
+    this.id = deliveryId++
+    store.deliveries.push(this)
+  }
+
+    meal() {
+      return store.meals.find(meal => meal.id === this.mealId)
+    }
+
+    neighborhood() {
+      return store.neighborhoods.find(neighborhood => neighborhood.id === this.neighborhoodId)
+    }
+
+    customer() {
+      return store.customers.find(customer => customer.id === this.customerId)
+    }
+
+
+}
