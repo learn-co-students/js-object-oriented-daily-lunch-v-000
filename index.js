78f418b7cc49db54b94c06fb15180f42d1761fae
github username <github email address>
2018-07-16 14:49:43 -0700

    neighborhood class still needs meals(), all other classes need to be created

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+    store.neighborhoods.push(this);
+  }
+
+  deliveries(){
+    return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+  }
+
+  customers(){
+    return store.customers.filter(customer => customer.neighborhoodId === this.id);
+  }
+
+  meals(){
+
+  }
+}
+
+
+class Customer {
+
+}
