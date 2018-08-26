aa1e917dfd264e01af99a66c0c8bbd563c45c0a1
taryn yaeger <tsyaeger@gmail.com>
2018-08-26 05:41:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodID = 0
+
+class Neighborhood {
+  constructor (name) {
+    this.name = name
+    this.id = neighborhoodID++
+  }
+  deliveries() {
+    return store.deliveries.filter(delivery => delivery.neighborhoodID === this.id)
+  }
+}
+
+
+
+// A meal has many customers
+// A delivery belongs to a meal, belongs to a customer, and belongs to a neighborhood
+// A customer has many deliveries
+// A customer has many meals through deliveries
+// A customer belongs to a neighborhood
+// A neighborhood has many deliveries
+// A neighborhood has many customers through deliveries
+// A neighborhood has many meals through deliveries
