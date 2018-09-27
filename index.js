8e19eb332e401880940b352a491f9236cdb40678
Naomi Dennis <njdennis94@gmail.com>
2018-09-27 18:10:57 +0000

    Complete Customer and Meal Class Test

--- index.js
+++ index.js
@@ -1,2 +1,73 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+function addMeToStore(){
+    this.id = store[this.constructor.name.toLowerCase() + "s"].push(this) - 1
+}
+
+class Neighborhood{
+  constructor(name){
+    this.name = name
+    addMeToStore.call(this)
+  }
+  deliveries(){
+    return store.deliveries.filter( (delivery) => { return delivery.neighborhoodId === this.id })
+  }
+  customers(){
+      return store.customers.filter( (customer) => { return customer.neighborhoodId === this.id })
+  }
+  meals(){
+    return store.meals.filter( (meal) => { return meal.neighborhoodId === this.id })
+ }
+}
+class Customer{
+  constructor(name, neighborhood){
+    this.name = name
+    this.neighborhoodId = neighborhood.id || 0
+    this.mealId = 0;
+    addMeToStore.call(this)
+  }
+  deliveries(){
+    return store.deliveries.filter( (delivery) => { return delivery.customerId === this.id })
+  }
+  meals(){
+    return this.deliveries().map( (myDelivery) => { return myDelivery.meal() })
+  }
+}
+
+class Meal{
+  constructor(title, price){
+    this.title = title
+    this.price = price
+    addMeToStore.call(this)
+  }
+ deliveries(){
+    return store.deliveries.filter( (delivery) => { return delivery.mealId === this.id })
+  }
+  customers(){
+    return store.customers.filter( (customer) => { return customer.mealId == this.id } )
+  }
+  static byPrice(){
+    return store.meals.sort((a,b) => { return b.price - a.price }) 
+    }
+
+}
+class Delivery{
+  constructor(meal, neighborhood, customer){
+    this.mealId = (meal) ? meal : 0
+    this.customerId = (customer) ? customer : 0
+    this.neighborhoodId = (neighborhood) ? neighborhood : 0
+    this.id = store["deliveries"].push(this) - 1
+  }
+  neighborhood(){
+      return store.neighborhoods.filter( (neighborhood) => { return neighborhood.neighborhoodId === this.id })
+  }
+  meal(){
+    return store.meals.find( (meal) => { return meal.id === this.mealId})
+  }
+  customer(){
+    return store.customers.find( (customer) => { return customer.id === this.customerId})
+  }
+  neighborhood(){
+    return store.neighborhoods.find( (neighborhood) => { return neighborhood.id === this.neighborhoodId})
+  }
+}
