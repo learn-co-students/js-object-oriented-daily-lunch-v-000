dc6f4a78e5c1bab376aa9ab9d06aa8221b529176
charlie  lee <ctlee9096@gmail.com>
2018-12-29 05:16:51 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ class Neighborhood {
 debugger;
 		store.neighborhoods.push(this);
 	}
+  deliveries(){
+    // returns all unique deliveries associated with a particular neighborhood ‣
+   return store.deliveries.filter (//by calling filter method on values of store deliveries collection
+     function(delivery){//argument to filter method -> annoymous function of a delivery argument
+       return delivery.neighborhoodId === this.id//associated with a particular neighborhood
+       //by using returning values of strictly equiv. operators between 2 method calls (delivery.neighborhoodId, this.id)
+     }.bind(this)//bind the delivery outcome to this Neighborhood
+   );
+  }
+  customers(){
+    return store.customers.filter(
+      function(customer){
+        return customer.neighborhoodId === this.id
+        debugger;
+      }.bind(this)
+    )
+  }
 }
 class Customer {
 	constructor(name, neighborhoodId) {
@@ -39,4 +56,26 @@ class Delivery {
 
 		store.deliveries.push(this);
 	}
+  meal() {
+    return store.meals.find(//from find method call on store meals collection;"COMPLETE"
+      function(meal){//from object of annoymous function with meal argument;"READ UP"
+        return meal.id === this.mealId;//returns the meal instance associated with a particular delivery; delivery belongs to a meal;"READ UP"
+      }.bind(this)//bind this meal object to this Delivery
+    )
+  }
+  customer () {
+    return store.customers.find(//from find method call on store customers collection;"COMPLETE"
+    function(customer){//pass in fn object argment to find method call->object of annoymous function with customer argument;"READ UP"
+      return customer.id === this.customerId;//returns the customer instance associated with a particular delivery; delivery belongs to a customer
+    }.bind(this)
+    )
+  }
+ neighborhood(){
+   return store.neighborhoods.find(
+     function(neighborhood){
+     return neighborhood.id === this.neighborhoodId;//returns the neighborhood in which a delivery was placed
+    }.bind(this)
+  )
+ }
+
 }
