6aabfd6699b141edadabd0dcddaf01597a81d986
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-11 23:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let mealId = 0;
+let neighborhoodId = 0;
+let customerId = 0;
+let deliveryId =  0;
+
+
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+    store.meals.push(this);
+  }
+}
+
+class Neighborhood {
+  constructor(name) {
+    this.id = ++neighborhoodId;
+    this.name = name;
+    store.neighborhoods.push(this);
+  }
+  deliveries() {
+    return store.deliveries.filter(function(delivery) {
+      return delivery.neighborhoodId === this.id
+    }.bind(this));
+  }
+}
+
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.id = ++customerId;
+    this.name = name;
+    this.neighborhoodId = neighborhoodId;
+    store.customers.push(this);
+  }
+}
+
+class Delivery {
+  constructor(mealId, customerId, neighborhoodId) {
+    this.id = ++deliveryId
+    this.mealId = mealId;
+    this.customerId = customerId;
+    this.neighborhoodId = neighborhoodId;
+    store.deliveries.push(this);
+  }
+
+}
