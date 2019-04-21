4e92386b19967e709554fb592fd5a9e86fcb0dd2
Rachel Wyatt <rw126@evansville.edu>
2019-04-21 17:30:58 -0400

    constructors for neighborhood, customer, meal, delivery

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
-// global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0
+let mealId = 0
+let customerId = 0
+let deliveryId = 0
+
+class Neighborhood {
+    constructor(name){
+      this.name = name;
+      this.id = ++neighborhoodId;
+      store.neighborhoods.push(this);
+    }
+}
+
+class Meal {
+    constructor(title, price = 0){
+      this.title = title;
+      this.price = price;
+      this.id = ++mealId;
+      store.meals.push(this);
+    }
+}
+class Customer {
+  constructor(name, neighborhoodId){
+    this.name = name;
+    this.id = ++customerId;
+    this.neighborhoodId = neighborhoodId;
+    store.customers.push(this);
+  }
+}
+
+class Delivery {
+  constructor(mealId, neighborhoodId, customerId){
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+    this.mealId = mealId;
+    this.id = ++deliveryId;
+    store.deliveries.push(this);
+    }
+}
+    
