3584ccd5cedc6b5c8a81b6f8912c46edb0881ec6
corytveten <corytveten@gmail.com>
2019-04-28 10:35:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+
+class Neighborhood {
+    constructor(name) {
+        this.id = ++neighborhoodId;
+        this.name = name;
+
+        store.neighborhoods.push(this);
+    };
+}
+
+let customerId = 0;
+
+class Customer {
+    constructor(name, neighborhood) {
+        this.id = ++customerId;
+        this.name = name;
+        this.neighborhoodId = neighborhood.id;
+
+        store.customers.push(this);
+    };
+}
+
+let mealId = 0;
+
+class Meal {
+    constructor(title, price) {
+        this.id = ++mealId;
+        this.title = title;
+        this.price = price;
+
+        store.meals.push(this);
+    };
+}
+
+let deliveryId = 0;
+
+class Delivery {
+    constructor(name) {
+        this.id = ++neighborhoodId;
+        this.name = name;
+
+        store.deliveries.push(this);
+    };
+}
+
