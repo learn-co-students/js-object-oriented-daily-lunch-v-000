2f92ef0886c776b8d2f2c96cc6c6920d1c01d1d9
RonSala <ron.sala@gmail.com>
2020-03-08 19:49:02 -0400

    Done.

--- index.js
+++ index.js
@@ -9,19 +9,41 @@ let deliveryId = 0;
 class Neighborhood {
   constructor(name) {
     this.name = name;
+    this.id = neighborhoodId ++;
+    store.neighborhoods.push(this);
   }
+
+  // deliveries() {
+  //   return this.
+  // }
 }
 
 class Customer {
-  constructor(name) {
+  constructor(name, neighborhoodId) {
     this.name = name;
+    this.neighborhoodId = neighborhoodId;
+    this.id = customerId ++;
+    store.customers.push(this);
   }
 }
 
 class Meal {
-
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+    this.mealId = mealId;
+    this.id = mealId ++;
+    store.meals.push(this);
+  }
 }
 
 class Delivery {
-
+  constructor(mealId, neighborhoodId, customerId) {
+    // debugger;
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+    this.id = deliveryId ++;
+    store.deliveries.push(this);
+  }
 }
