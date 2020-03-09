b80e61d9b0c0746ced5d2aa0019754d26f3c8722
RonSala <ron.sala@gmail.com>
2020-03-08 20:52:42 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let customerId = 0;
 let mealId = 0;
 let deliveryId = 0;
 
+// has many deliveries
+// has many customers through deliveries
+// has many meals through deliveries
 class Neighborhood {
   constructor(name) {
     this.name = name;
@@ -13,11 +16,22 @@ class Neighborhood {
     store.neighborhoods.push(this);
   }
 
-  // deliveries() {
-  //   return this.
-  // }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId == this.id;
+    });
+  }
+
+  customers() {
+    return store.customers.filter(customer => {
+      return customer.neighborhoodId == this.id;
+    });
+  }
 }
 
+// has many deliveries
+// has many meals through deliveries
+// belongs to a neighborhood
 class Customer {
   constructor(name, neighborhoodId) {
     this.name = name;
@@ -25,25 +39,39 @@ class Customer {
     this.id = customerId ++;
     store.customers.push(this);
   }
+
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      this.id == delivery.customerId;
+    })
+  }
 }
 
+//has many customers
 class Meal {
   constructor(title, price) {
     this.title = title;
     this.price = price;
-    this.mealId = mealId;
     this.id = mealId ++;
     store.meals.push(this);
   }
 }
 
+// belongs to a meal, belongs to a customer, and belongs to a neighborhood
 class Delivery {
   constructor(mealId, neighborhoodId, customerId) {
-    // debugger;
     this.mealId = mealId;
     this.neighborhoodId = neighborhoodId;
     this.customerId = customerId;
     this.id = deliveryId ++;
     store.deliveries.push(this);
+  }
+
+  meal() {
+    let m = store.meals.filter(meal => {
+      return meal.deliveryId == this.id;
+    });
+debugger
+    return Object.assign({}, m);
   }
 }
